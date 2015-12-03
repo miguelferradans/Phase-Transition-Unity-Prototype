0d5bae0da014ead437f79f250e1bdd45127ef001
Esteban <estebanhs91@hotmail.com>
2015-12-03 12:39:16 +0100

    añadida barra de tiempo

---
+++ Assets/Scripts/Player/TimeCounter.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class TimeCounter : MonoBehaviour
+{
+    [Header("TimeFields")]
+    [SerializeField]
+    [Range(0.0f, 5.0f)]
+    private float _totalTime = 5f;
+    [SerializeField]
+    [Range(0.0f, 5.0f)]
+    private float _timeRemaining;
+    [SerializeField]
+    private Slider _timeBar;
+
+    // Use this for initialization
+    void Start()
+    {
+        _timeBar.maxValue = _totalTime;
+        _timeBar.minValue = 0;
+        _timeBar.value = _timeRemaining;
+    }
+
+    //------------------------------------------METHODS-----------------------------------------------------
+    /// <summary>   ConsumeTime()
+    ///     
+    /// </summary>
+    void ConsumeTime()
+    {
+        _timeBar.value -= Time.deltaTime;
+        if (_timeBar.value == 0)
+            SendMessage("TimeLeft", false, SendMessageOptions.RequireReceiver);
+    }
+    /// <summary>   RecoverTime()
+    ///     
+    /// </summary>
+    void RecoverTime()
+    {
+        _timeBar.value = _totalTime;
+    }
+}
+
