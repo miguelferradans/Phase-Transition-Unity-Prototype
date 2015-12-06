fb5903e821d8dd2fac26553f89b9745138478631
Esteban <estebanhs91@hotmail.com>
2015-12-06 13:19:09 +0100

    level3

---
+++ Assets/Scripts/Interactuable Elements/FanRotation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FanRotation : MonoBehaviour
+{    
+        void Update()
+        {
+            transform.Rotate(new Vector3(0, 1, 0), 500 * Time.deltaTime);
+        }   
+}
